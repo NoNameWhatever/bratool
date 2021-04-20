a5cf56ec7139044522957d1ec64e55423ce3349b
Fredrik Sundberg <sundrik@gmail.com>
2021-04-20 20:41:22 +0200

    Added POST, PUT, PATCH and DELETE for testing

--- backend/django-app/tools/views.py
+++ backend/django-app/tools/views.py
@@ -1,12 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from tools import serializers
 
 
 class ToolApiView(APIView):
     """Tool API View"""
+    serializer_class = serializers.ToolsSerializer
 
     def get(self, request, format=None):
-        """Return a list of tools"""
+        """Return a list of tools"""        
 
         an_apiview = [
             'Uses HTTP methods as function (ger, post, patch, put, delete)',
@@ -16,3 +20,29 @@ class ToolApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a Hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """"Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
